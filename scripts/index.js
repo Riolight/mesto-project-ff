bbf186418e5e2bd616d394215b4ce35f58f44b6e
Roman Bakurov <ibakur@yandex.ru>
2024-05-29 09:51:25 +0300

    add new functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
+const content = document.querySelector(".content");
+const cardsConteiner = content.querySelector(".places__list");
+
+initialCards.forEach(function (element) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = element.link;
+  cardElement.querySelector(".card__title").textContent = element.name;
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function (evt) {
+      let deleteCard = evt.target.classList.toggle(".places__item");
+      cardElement.remove(deleteCard);
+    });
+
+  cardsConteiner.append(cardElement);
+});
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
